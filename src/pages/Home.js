854800d9e35d183dabd0d2ae6b823861f0a7325f
nikkylu <nikkylu@foxmail.com>
2023-06-27 17:14:45 +0800

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+const Home = () => {
+  return (
+  <div className="Home">
+    <h2>Bio</h2>
+    <div>Nikky is living and working in New York City. </div>
+    <div>Born 2003, Hangzhou, China.</div>
+  </div>
+  )
+};
+
+export default Home;
---
+++ src/pages/Layout.js
@@ -0,0 +1,35 @@
+import { Outlet, Link } from "react-router-dom";
+import "./Layout.css"
+
+const Layout = () => {
+  return (
+    <div className="Layout">
+      <div>
+        <div>
+          <div className="Layout-bar">
+            <div>Home</div>
+            <Link to="/" className="Layout-sidebar">Bio</Link>
+          </div>
+          <div className="Layout-bar">
+            <div>Photo</div>
+            <Link to="/photo" className="Layout-sidebar">We Stare into the World and See Ourselves</Link>
+            <Link to="/photo" className="Layout-sidebar">Shot on iPhone</Link>
+          </div>
+          <div className="Layout-bar">
+            <div>Blogs</div>
+            <Link to="/blogs" className="Layout-sidebar">Random Thoughts</Link>
+          </div>
+          <div>
+            <Link to="/contact" className="Layout-bar">Contact</Link>
+            <div className="Layout-sidebar">nikkylu.zl@gmail.com</div>
+          </div>
+        </div>
+        <div className="Layout-sidebar">© Nikky Lu 2023 All Rights Reserved</div>
+      </div>
+
+      <Outlet />
+    </div>
+  )
+};
+
+export default Layout;
